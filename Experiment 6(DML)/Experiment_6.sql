31ab51fcb6f96d3ed93549f6cf7e2ba667c4ce80
Shivani Jagannatham <69143386+19wh1a12b0@users.noreply.github.com>
2021-06-22 23:20:52 +0530

    Create Experiment_6.sql

---
+++ Experiment 6(DML)/Experiment_6.sql
@@ -0,0 +1,40 @@
+CREATE SCHEMA 19WH1A12B0;
+USE 19WH1A12B0;
+CREATE TABLE Bus(bus_no VARCHAR(20),Sources VARCHAR(20),Destination VARCHAR(20));
+INSERT INTO Bus VALUES('TS121','Hyd','Chennai');
+INSERT INTO Bus VALUES('TS122','Pune',   'Mumbai');
+INSERT INTO Bus VALUES('TS123','Ooty',  'Chennai');
+INSERT INTO Bus VALUES('TS124','Vizag','Banglore');
+SELECT * FROM Bus;
+
+DELETE FROM Bus b WHERE b.Sources ='Hyd';
+SELECT * FROM Bus;
+
+UPDATE Bus SET Destination ='Tirupati' WHERE bus_no = 'TS121';
+SELECT * FROM Bus;
+
+CREATE TABLE Passengers(P_name VARCHAR(20),P_id INT,Gender VARCHAR(20),Age INT,P_destination VARCHAR(20));
+INSERT INTO Passengers VALUES('Shila',123,'Female',22,'Bharagiri');
+INSERT INTO Passengers VALUES('Raghu',456,'Male',40,'Banglore');
+INSERT INTO Passengers VALUES('Geetha',789,'Female',20,'Ooty');
+INSERT INTO Passengers VALUES('Ramesh',101,'Male',32,'chennai');
+
+SELECT * FROM Passengers;
+
+DELETE FROM Passengers P  WHERE P_name = 'Geetha';
+SELECT * FROM Passengers;
+
+UPDATE Passengers SET P_name ='Ramesh' WHERE P_id = '852';
+SELECT * FROM Passengers;
+
+CREATE TABLE Ticket(T_id int, Sources VARCHAR(20),  Destination VARCHAR(20),P_id int);
+INSERT INTO Ticket VALUES(1,'Hyd','Banglore',123);
+INSERT INTO Ticket VALUES(2,'Pune','Mumbai',456);
+INSERT INTO Ticket VALUES(3,'Ooty','Chennai',789);
+INSERT INTO Ticket VALUES(4,'Hyd','Vizag',101);
+
+DELETE FROM Ticket T  WHERE Sources = 'Ooty';
+SELECT * FROM Ticket;
+
+UPDATE Ticket SET Destination  ='Kodaikanal' WHERE P_id = '789';
+SELECT * FROM Ticket;
